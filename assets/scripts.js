4e755c74e72a907629efe137dd28f8cfb2d53cd4
viruzx <slava@knyz.org>
2015-02-07 17:48:20 -0500

    Alpha Concept Release
    A small portion of work is complete.

---
+++ assets/scripts.js
@@ -0,0 +1,26 @@
+ var documentHeight = 0;
+  var topPadding = 15;
+  $(function() {
+      var offset = $("#cssmenu").offset();
+      documentHeight = $(document).height();
+      $(window).scroll(function() {
+          var sideBarHeight = $("#cssmenu").height();
+          if ($(window).scrollTop() > offset.top) {
+              var newPosition = ($(window).scrollTop() - offset.top) + topPadding;
+              $("#cssmenu").stop().animate({
+                  marginTop: newPosition - 15
+              });
+          } else {
+              $("#cssmenu").stop().animate({
+                  marginTop: 0
+              });
+          };
+      });
+  });
+
+$( "#menulabel" ).click(function() {
+  $("#cssmenu").toggleClass( "show" );
+});
+$( "#cssmenu" ).mouseout(function() {
+  $("#cssmenu").toggleClass( "show" );
+});
